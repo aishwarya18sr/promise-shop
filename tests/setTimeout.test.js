740ba8a05a1c7c06a572b16383f047151b51e2c7
aishwarya18sr <Aishwarya_S_R@mckinsey.com>
2022-02-18 08:45:04 +0530

    feat: add few more unit testcases for setTimeout program

--- tests/setTimeout.test.js
+++ tests/setTimeout.test.js
@@ -1,10 +1,19 @@
 const timer = require('../setTimeout.js');
 
 describe('timer function', function() {
-    it.only('should execute a function after 1700 ms', () => {
+    it('should execute a function after 1700 ms', () => {
       const spy = jest.spyOn(global, 'setTimeout');
       timer();    
-      expect(spy).toHaveBeenCalledTimes(1);
       expect(spy).toHaveBeenLastCalledWith(expect.any(Function), 1700);
+    });
+    it('should not execute a function after 1700 ms', () => {
+      const spy = jest.spyOn(global, 'setTimeout');
+      timer();    
+      expect(spy).not.toHaveBeenLastCalledWith(expect.any(Function), 1800);
+    });
+    it('should not execute a function before 1700 ms', () => {
+      const spy = jest.spyOn(global, 'setTimeout');
+      timer();    
+      expect(spy).not.toHaveBeenLastCalledWith(expect.any(Function), 1600);
     });
 });
